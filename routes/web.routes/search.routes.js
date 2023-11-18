975bfa709e2572af04aa05d58fcf0aa06674c7cc
javi <fordrsmax@gmail.com>
2023-11-18 20:34:24 +0100

    fix metod call name

--- routes/web.routes/search.routes.js
+++ routes/web.routes/search.routes.js
@@ -1,7 +1,7 @@
 const searchRouter = require('express').Router()
 const searchController = require('../../controllers/search.controller')
 
-searchRouter.get('/search',searchController.searchMovieBytitle)
+searchRouter.get('/search',searchController.displaySearchScreen)
 
 //este metodo usara el util fetch y la ruta de busqueda de mongo
 searchRouter.get('/search/:title',searchController.searchMovieBytitle)
