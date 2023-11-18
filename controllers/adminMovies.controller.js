c3a3d0e652a3adf0208b8f3fd79c45e9b7dbfee5
javi <fordrsmax@gmail.com>
2023-11-18 20:23:58 +0100

    add basic controllers files ,  TODO methods

---
+++ controllers/adminMovies.controller.js
@@ -0,0 +1,28 @@
+
+const searchAdminMovie = (req,res)=>{
+
+}
+
+const readAdminMovies = (req,res)=>{
+
+}
+
+const createAdminMovies = (req,res)=>{
+
+}
+
+const updateAdminMovies = (req,res)=>{
+
+}
+
+const deleteAdminMovies = (req,res)=>{
+
+}
+
+module.exports = {
+    searchAdminMovie,
+    readAdminMovies,
+    createAdminMovies,
+    updateAdminMovies,
+    deleteAdminMovies    
+}
