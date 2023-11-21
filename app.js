ea9d1d7d4e8d0fd288d4248d58af04330d933567
unknown <carloschinchillamoreno@gmail.com>
2023-11-21 12:06:48 +0100

    Add css to results and details view

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //for testing scafolding , later move to corresponding  sql controller
-require('./config/db_pgsql');
-require('./config/db_mongo');
+//require('./config/db_pgsql');
+//require('./config/db_mongo');
 
 const express = require('express')
 const app = express()
