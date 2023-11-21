b47b37dd1593e86d3dc42e8d187f32d3f043744a
juliaisasti <144235263+juliaisasti@users.noreply.github.com>
2023-11-21 12:13:39 +0100

    users controller with SQL finished

--- routes/api.routes/user.routes.js
+++ routes/api.routes/user.routes.js
@@ -1,20 +1,22 @@
 // CRUD USERS
 const userRoutes = require('express').Router()
 
-const usersController = require('../../controllers/user.controller')
+const usersController = require('../../controllers/users.controller')
 
+//[GET] /getAllUsers
+userRoutes.get('/user/all', usersController.getAllUsers)
 
 //[GET]/readUser - SQL
-userRoutes.get('/user',usersController.readUser)
+userRoutes.get('/user/:id',usersController.readUser)
 
 //[POST] /createUser - SQL 
-userRoutes.post('/user',usersController.createUser)
+userRoutes.post('/user/createUser',usersController.createUser)
 
-//[PUT] /editUser - SQL 
-userRoutes.put('/user',usersController.updateUser) // no se si se va a usar
+ //[PUT] /editUser - SQL 
+ userRoutes.put('/user/updateUser',usersController.updateUser) // no se si se va a usar
 
 //[DELETE] /removeUser - SQL 
-userRoutes.delete('/user',usersController.deleteUser)
+userRoutes.delete('/user/deleteUser',usersController.deleteUser)
 
 
 
