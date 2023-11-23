e3cc0ec31fd1f30fbe51714cbf92f92f654510ea
juliaisasti <144235263+juliaisasti@users.noreply.github.com>
2023-11-23 10:36:40 +0100

    minor fixesç

--- config/db_pgsql.js
+++ config/db_pgsql.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
 require('dotenv').config()
-//?sslmode=rquire
-const dbURI = `postgres://${process.env.SQL_USER}:${process.env.SQL_PASSWORD}@${process.env.SQL_HOST}/${process.env.SQL_NAME}?sslmode=rquire`
+
+const dbURI = `postgres://${process.env.SQL_USER}:${process.env.SQL_PASSWORD}@${process.env.SQL_HOST}/${process.env.SQL_NAME}?sslmode=require`
 
 const db = new Sequelize(dbURI);
 
