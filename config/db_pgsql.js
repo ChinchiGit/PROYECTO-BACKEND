3f8dc67501e2cca794c00667b621bfcd3cb87a22
javi <fordrsmax@gmail.com>
2023-11-23 12:11:37 +0100

    edit config sql , loggin false

--- config/db_pgsql.js
+++ config/db_pgsql.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 //?sslmode=rquire
 const dbURI = `postgres://${process.env.SQL_USER}:${process.env.SQL_PASSWORD}@${process.env.SQL_HOST}/${process.env.SQL_NAME}?sslmode=rquire`
 
-const db = new Sequelize(dbURI);
+const db = new Sequelize(dbURI,{logging:false});
 
 const connectSQL = async () => {
     try {
@@ -15,8 +15,7 @@ const connectSQL = async () => {
     }
 };
 
-//connectSQL();
-
+connectSQL();
 
 module.exports = {
     connectSQL,
