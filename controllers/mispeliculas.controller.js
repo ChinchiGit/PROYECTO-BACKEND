4d0cbfc2815c0406aa3e82178351e1f31baafe3c
javi <fordrsmax@gmail.com>
2023-11-24 09:35:46 +0100

    Revert "hotfix"
    This reverts commit 225bf0425908b72bb0bfac2f53bdb74654268aed.

--- controllers/mispeliculas.controller.js
+++ controllers/mispeliculas.controller.js
@@ -92,7 +92,7 @@ const deleteFavMovie = async (req, res) => {
     // const id = data.id_user;
     // if (id) {
     let borrar = await misPeliculasModel.destroy({where: { idUser: decoded.id_user, idFavMovie: data}});
-    res.redirect("/api/mispeliculas");
+    res.render("user_mispeliculas");
     //   if (result.deletedCount == 0)
     //     res.status(400).json({ message: `User con ID ${id} no encontrado` });
     //   else
